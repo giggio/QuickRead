729bf409da027dfc44a6036ba5f66842ca3e190f
Giovanni Bassi <giggio@giggio.net>
2014-03-16 23:06:09 -0300

    enabling to get an url

--- QuickRead/js/default.js
+++ QuickRead/js/default.js
@@ -6,6 +6,8 @@
     "use strict";
 
     function display(text) {
+        $("#controlButtons").show();
+        $("#urlBlock").hide();
         jetzt.config("target_wpm", 500);
         jetzt.init(text);
         $(".sr-blackout").hide();
@@ -25,6 +27,57 @@
             return jetzt.adjustWPM(-10);
         });
     }
+    function getArticleFromUri(uri) {
+        return WinJS.xhr({
+            type: 'get',
+            url: 'http://boilerpipe-web.appspot.com/extract?extractor=DefaultExtractor&output=json&extractImages=&url=' + uri,
+            data: null
+        }).then(function (res) {
+            var parsedPage = JSON.parse(res.responseText);
+            return parsedPage.response.content;
+        });
+    }
+
+    function doShareWithPlainText(shareOperation) {
+        return shareOperation.data.getTextAsync().then(function (text) {
+            display(text);
+            shareOperation.reportDataRetrieved();
+        });
+    }
+
+    function doShareWithUri(shareOperation) {
+        return shareOperation.data.getUriAsync().then(function (uri) {
+            return getArticleFromUri(uri.absoluteUri);
+        }).then(function (articleText) {
+            display(articleText);
+            shareOperation.reportDataRetrieved();
+        });
+    }
+
+    function doShareWithHtml(shareOperation) {
+        return shareOperation.data.getHtmlFormatAsync().then(function (htmlFormat) {
+            var htmlFragment = Windows.ApplicationModel.DataTransfer.HtmlFormatHelper.getStaticFragment(htmlFormat);
+            if (htmlFragment.trim().toLowerCase().substr(0, 5) == "<html" && shareOperation.data.contains(Windows.ApplicationModel.DataTransfer.StandardDataFormats.uri)) {
+                return doShareWithUri(shareOperation);
+            } else {
+                var el = document.createElement("div");
+                el.innerHTML = htmlFragment;
+                var text = el.textContent;
+                display(text);
+            }
+        });
+    }
+
+    function doShare(shareOperation) {
+        if (shareOperation.data.contains(Windows.ApplicationModel.DataTransfer.StandardDataFormats.text)) {
+            return doShareWithPlainText(shareOperation);
+        } else if (shareOperation.data.contains(Windows.ApplicationModel.DataTransfer.StandardDataFormats.html)) {
+            return doShareWithHtml(shareOperation);
+        } else if (shareOperation.data.contains(Windows.ApplicationModel.DataTransfer.StandardDataFormats.uri)) {
+            return doShareWithUri(shareOperation);
+        }
+        return undefined;
+    }
 
     var app = WinJS.Application;
     var activation = Windows.ApplicationModel.Activation;
@@ -32,29 +85,17 @@
     app.onactivated = function (args) {
         if (args.detail.kind === Windows.ApplicationModel.Activation.ActivationKind.shareTarget) {
             var shareOperation = args.detail.shareOperation;
-            if (shareOperation.data.contains(Windows.ApplicationModel.DataTransfer.StandardDataFormats.text)) {
-                shareOperation.data.getTextAsync().done(function (text) {
-                    display(text);
-                    shareOperation.reportDataRetrieved();
-                }, function (e) {
-                    shareOperation.reportError("Error: " + e);
-                });
-            } else if (shareOperation.data.contains(Windows.ApplicationModel.DataTransfer.StandardDataFormats.html)) {
-                shareOperation.data.getHtmlFormatAsync().then(function (htmlFormat) {
-                    var htmlFragment = Windows.ApplicationModel.DataTransfer.HtmlFormatHelper.getStaticFragment(htmlFormat);
-                    var el = document.createElement("div");
-                    el.innerHTML = htmlFragment;
-                    var text = el.textContent;
-                    display(text);
-                    shareOperation.reportDataRetrieved();
-                });
-            }
+            doShare(args.detail.shareOperation).done(function () {
+                return shareOperation.reportDataRetrieved();
+            }, function (e) {
+                return shareOperation.reportError("Error: " + e.Error);
+            });
         }
         if (args.detail.kind === activation.ActivationKind.launch) {
             if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
                 // TODO: This application has been newly launched. Initialize
                 // your application here.
-                display("Elit perspiciatis deserunt eveniet (maxime) tempore vero \"similique\" similique delectus nobis! Aliquid ut amet doloremque sunt vitae harum aliquam vel, eos! Ipsam assumenda illo architecto saepe accusantium. Cumque ipsam nemo?");
+                //display("Elit perspiciatis deserunt eveniet (maxime) tempore vero \"similique\" similique delectus nobis! Aliquid ut amet doloremque sunt vitae harum aliquam vel, eos! Ipsam assumenda illo architecto saepe accusantium. Cumque ipsam nemo?");
             } else {
                 // TODO: This application has been reactivated from suspension.
                 // Restore application state here.
@@ -73,4 +114,26 @@
     };
 
     app.start();
+
+    $(function () {
+        $("#doGetUrl").click(function () {
+            var url = $("#url").val().trim();
+            var expressionWithHttp = '^(https?:\/\/)([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?';
+            var regexWithHttp = new RegExp(expressionWithHttp, "i");
+            var expressionWithoutHttp = '([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?';
+            var regexWithoutHttp = new RegExp(expressionWithoutHttp, "gi");
+            var messageDialog;
+            if (url === "") {
+                messageDialog = new Windows.UI.Popups.MessageDialog("Please type a url, starting with http.");
+                return messageDialog.showAsync();
+            } else if (url.match(regexWithHttp)) {
+                getArticleFromUri(url).then(display);
+            } else if (url.match(regexWithoutHttp)) {
+                getArticleFromUri("http://" + url).then(display);
+            } else {
+                messageDialog = new Windows.UI.Popups.MessageDialog("Please select an acceptable (starting with http, www, or at least a correct url).");
+                return messageDialog.showAsync();
+            }
+        });
+    });
 })();
--- QuickRead/js/default.ts
+++ QuickRead/js/default.ts
@@ -2,16 +2,19 @@
 /// <reference path="typings/jquery/jquery.d.ts" />
 /// <reference path="typings/patches.d.ts" />
 /// <reference path="typings/jetzt/jetzt.d.ts" />
-(function() {
+declare var readability: any;
+(() => {
   "use strict";
 
   function display(text) {
+    $("#controlButtons").show();
+    $("#urlBlock").hide();
     jetzt.config("target_wpm", 500);
     jetzt.init(text);
     $(".sr-blackout").hide();
-    $("#toggleRunning").click(function() {
+    $("#toggleRunning").click(function () {
       var running = this.value === "Pause";
-      this.value = running? "Play" : "Pause";
+      this.value = running ? "Play" : "Pause";
       jetzt.toggleRunning(!running);
     });
     $("#nextParagraph").click(jetzt.nextParagraph);
@@ -19,7 +22,58 @@
     $("#previousParagraph").click(jetzt.prevParagraph);
     $("#previousSentence").click(jetzt.prevSentence);
     $("#faster").click(() => jetzt.adjustWPM(10));
-    $("#slower").click(()=> jetzt.adjustWPM(-10));
+    $("#slower").click(() => jetzt.adjustWPM(-10));
+  }
+  function getArticleFromUri(uri: string) :  Windows.Foundation.IPromise<string> {
+    return WinJS.xhr({
+      type: 'get',
+      url: 'http://boilerpipe-web.appspot.com/extract?extractor=DefaultExtractor&output=json&extractImages=&url=' + uri,
+      data: null
+    }).then(res=> {
+        var parsedPage = JSON.parse(res.responseText);
+        return parsedPage.response.content;
+      });
+  }
+
+  function doShareWithPlainText(shareOperation: Windows.ApplicationModel.DataTransfer.ShareTarget.ShareOperation): Windows.Foundation.IPromise<any> {
+    return shareOperation.data.getTextAsync().then(text=> {
+      display(text);
+      shareOperation.reportDataRetrieved();
+    });
+  }
+
+  function doShareWithUri(shareOperation: Windows.ApplicationModel.DataTransfer.ShareTarget.ShareOperation): Windows.Foundation.IPromise<any> {
+    return shareOperation.data.getUriAsync()
+      .then(uri=> getArticleFromUri(uri.absoluteUri))
+      .then(articleText => {
+        display(articleText);
+        shareOperation.reportDataRetrieved();
+      });
+  }
+
+  function doShareWithHtml(shareOperation: Windows.ApplicationModel.DataTransfer.ShareTarget.ShareOperation): Windows.Foundation.IPromise<any> {
+    return shareOperation.data.getHtmlFormatAsync().then(htmlFormat=> {
+      var htmlFragment = Windows.ApplicationModel.DataTransfer.HtmlFormatHelper.getStaticFragment(htmlFormat);
+      if (htmlFragment.trim().toLowerCase().substr(0, 5) == "<html" && shareOperation.data.contains(Windows.ApplicationModel.DataTransfer.StandardDataFormats.uri)) {
+        return doShareWithUri(shareOperation);
+      } else {
+        var el = document.createElement("div");
+        el.innerHTML = htmlFragment;
+        var text = el.textContent;
+        display(text);
+      }
+    });
+  }
+
+  function doShare(shareOperation: Windows.ApplicationModel.DataTransfer.ShareTarget.ShareOperation): Windows.Foundation.IPromise<any> {
+    if (shareOperation.data.contains(Windows.ApplicationModel.DataTransfer.StandardDataFormats.text)) {
+      return doShareWithPlainText(shareOperation);
+    } else if (shareOperation.data.contains(Windows.ApplicationModel.DataTransfer.StandardDataFormats.html)) {
+      return doShareWithHtml(shareOperation);
+    } else if (shareOperation.data.contains(Windows.ApplicationModel.DataTransfer.StandardDataFormats.uri)) {
+      return doShareWithUri(shareOperation);
+    }
+    return undefined;
   }
 
   var app = WinJS.Application;
@@ -28,29 +82,17 @@
   app.onactivated = args => {
     if (args.detail.kind === Windows.ApplicationModel.Activation.ActivationKind.shareTarget) {
       var shareOperation = args.detail.shareOperation;
-      if (shareOperation.data.contains(Windows.ApplicationModel.DataTransfer.StandardDataFormats.text)) {
-        shareOperation.data.getTextAsync().done(text=> {
-          display(text);
-          shareOperation.reportDataRetrieved();
-        }, e=> {
-          shareOperation.reportError("Error: " + e);
-        });
-      } else if (shareOperation.data.contains(Windows.ApplicationModel.DataTransfer.StandardDataFormats.html)) {
-        shareOperation.data.getHtmlFormatAsync().then(htmlFormat=> {
-          var htmlFragment = Windows.ApplicationModel.DataTransfer.HtmlFormatHelper.getStaticFragment(htmlFormat);
-          var el = document.createElement("div");
-          el.innerHTML = htmlFragment;
-          var text = el.textContent;
-          display(text);
-          shareOperation.reportDataRetrieved();
-        });
-      }
+      doShare(args.detail.shareOperation)
+        .done(
+        () => shareOperation.reportDataRetrieved(),
+        e => shareOperation.reportError("Error: " + e.Error)
+        );
     }
     if (args.detail.kind === activation.ActivationKind.launch) {
       if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
         // TODO: This application has been newly launched. Initialize
         // your application here.
-        display("Elit perspiciatis deserunt eveniet (maxime) tempore vero \"similique\" similique delectus nobis! Aliquid ut amet doloremque sunt vitae harum aliquam vel, eos! Ipsam assumenda illo architecto saepe accusantium. Cumque ipsam nemo?");
+        //display("Elit perspiciatis deserunt eveniet (maxime) tempore vero \"similique\" similique delectus nobis! Aliquid ut amet doloremque sunt vitae harum aliquam vel, eos! Ipsam assumenda illo architecto saepe accusantium. Cumque ipsam nemo?");
       } else {
         // TODO: This application has been reactivated from suspension.
         // Restore application state here.
@@ -69,4 +111,27 @@
   };
 
   app.start();
+
+  $(()=> {
+    $("#doGetUrl").click(() => {
+      var url = $("#url").val().trim();
+      var expressionWithHttp = '^(https?:\/\/)([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?';
+      var regexWithHttp = new RegExp(expressionWithHttp, "i");
+      var expressionWithoutHttp = '([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?';
+      var regexWithoutHttp = new RegExp(expressionWithoutHttp, "gi");
+      var messageDialog:Windows.UI.Popups.MessageDialog;
+      if (url === "") {
+        messageDialog = new Windows.UI.Popups.MessageDialog("Please type a url, starting with http.");
+        return messageDialog.showAsync();
+      } else if (url.match(regexWithHttp)) {
+        getArticleFromUri(url).then(display);
+      } else if (url.match(regexWithoutHttp)) {
+        getArticleFromUri("http://" + url).then(display);
+      } else {
+        messageDialog = new Windows.UI.Popups.MessageDialog("Please select an acceptable (starting with http, www, or at least a correct url).");
+        return messageDialog.showAsync();
+      }
+    });
+  });
+
 })();
